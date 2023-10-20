a16a3e51063d5b0415f7b56360672bae55da6d6e
zhanknight <20487008+zhanknight@users.noreply.github.com>
2023-10-20 09:08:45 -0400

    create viewmodels and move entity models to folder

---
+++ TacoMusings.API/TacoMusings.API/Models/Views/AuthorView.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+namespace TacoMusings.API.Models;
+
+public class AuthorView
+{
+    [Key]
+    [Column("AuthorID")]
+    public int AuthorId { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    [Unicode(false)]
+    public string AuthorName { get; set; }
+
+    [Required]
+    [StringLength(500)]
+    [Unicode(false)]
+    public string AuthorBio { get; set; }
+
+    [Column("AuthorPhotoID")]
+    public int AuthorPhotoId { get; set; }
+
+    public virtual ICollection<ContentView> Content { get; set; } = new List<ContentView>();
+}
---
+++ TacoMusings.API/TacoMusings.API/Models/Views/ContentView.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+namespace TacoMusings.API.Models;
+
+public partial class ContentView
+{
+    [Key]
+    [Column("ContentID")]
+    public int ContentId { get; set; }
+
+    public int ContentAuthor { get; set; }
+
+    public int ContentType { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    [Unicode(false)]
+    public string ContentTitle { get; set; }
+
+    [Required]
+    [StringLength(1024)]
+    [Unicode(false)]
+    public string ContentBody { get; set; }
+   
+
+    [Column(TypeName = "datetime")]
+    public DateTime? ContentDate { get; set; }
+
+
+    [StringLength(255)]
+    [Unicode(false)]
+    public string? ContentSource { get; set; }
+
+
+    [ForeignKey("ContentAuthor")]
+    [InverseProperty("Content")]
+    public virtual Author? ContentAuthorNavigation { get; set; }
+
+
+    [ForeignKey("ContentType")]
+    [InverseProperty("Content")]
+    public virtual ContentType? ContentTypeNavigation { get; set; }
+
+    [InverseProperty("MappedContent")]
+    public virtual ICollection<TagMap> TagMap { get; set; } = new List<TagMap>();
+}
